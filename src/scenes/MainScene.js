ff8fe346cd6c2be6ca4b36ebfda56caae674294e
Ryan Kubik <rmkubik@me.com>
2019-03-13 20:25:55 -0700

    Add lifecycle methods to scene

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -1,6 +1,21 @@
 import Phaser from "phaser";
+import globals from "../globals";
+import donutSpriteSheet from "../../assets/donuts.png";
+import Donut from "../gameObjects/Donut";
+import Pipes from "../gameObjects/Pipes";
 
 class MainScene extends Phaser.Scene {
+  /**
+   * Scene lifecycle method, preload().
+   * This method is used to load and register assets into our game.
+   */
+  preload() {}
+
+  /**
+   * Scene lifecycle method, create().
+   * This is the only required lifecycle method for a Scene.
+   * It is used to initialize your game.
+   */
   create() {
     /**
      * Add a text Game Object to our Scene.
@@ -14,6 +29,13 @@ class MainScene extends Phaser.Scene {
      */
     this.add.text(10, 10, "Flappy Donut");
   }
+
+  /**
+   * Scene lifecycle method, update().
+   * This method is used to make changes as the game progresses.
+   * It is called when the Scene runs an update step.
+   */
+  update() {}
 }
 
 export default MainScene;
