21bf8a0c1be8581ee0e2739c3455cbb8ca385b85
Ryan Kubik <rmkubik@me.com>
2019-03-17 23:15:11 -0700

    Remove comment noise

--- src/gameObjects/Pipes.js
+++ src/gameObjects/Pipes.js
@@ -8,17 +8,7 @@ import globals from "../globals";
  * A Group is a way for you to create, manipulate, or recycle similar Game Objects.
  */
 class Pipes extends Phaser.GameObjects.Group {
-  /**
-   * @param {object} configuration
-   * @param {Phaser.Scene} configuration.scene - the Phaser.Scene that owns this Sprite
-   */
   constructor({ scene }) {
-    /**
-     * We need to call the Group constructor
-     *
-     * arguments:
-     * - the Phaser.Scene that owns this Group
-     */
     super(scene);
     scene.add.existing(this);
     this.scene = scene;
@@ -26,15 +16,6 @@ class Pipes extends Phaser.GameObjects.Group {
 
   /**
    * Creates a new Game Object and adds it to this group
-   *
-   * This is an extension of the default Phaser.GameObject.Group.create() function.
-   * We want to set a few defaults on our pipe sections after we add them to the Pipes Group.
-   *
-   * @param {object} configuration
-   * @param {number} configuration.x - coordinate in pixels
-   * @param {number} configuration.y - coordinate in pixels
-   * @param {string} configuration.frame - the frame to use for our pipe Sprite
-   * @param {boolean} configuration.isOnCeiling - should our pipe be attached to the ceiling
    */
   create({ x, y, frame, isOnCeiling }) {
     // const pipe = super.create(x, y, "donuts", frame);
@@ -48,8 +29,6 @@ class Pipes extends Phaser.GameObjects.Group {
 
   /**
    * Checks if the provided pipe section's physics body is off the bottom or left side of the screen
-   *
-   * @param {Phaser.GameObjects.Sprite} pipe - the piece of pipe we're checking
    */
   static isPipeOffScreen(pipe) {
     return pipe.body.x < 0 || pipe.body.y > globals.height;
@@ -57,9 +36,6 @@ class Pipes extends Phaser.GameObjects.Group {
 
   /**
    * Determine the state of our Pipes when the parent Scene is updated
-   *
-   * This isn't called automatically and needs to be called explicitly
-   * by our parent scene's update lifecycle method.
    */
   update() {
     // this.getChildren().forEach(pipe => {
@@ -73,30 +49,15 @@ class Pipes extends Phaser.GameObjects.Group {
 
   /**
    * Build a pipe on the screen that is either attached to the floor or the ceiling
-   *
-   * @param {number} height - how many tiles tall should our pipe be
-   * @param {boolean} [isOnCeiling=false] - should our pipe be attached to the ceiling
    */
   buildPipe(height, isOnCeiling = false) {
-    /**
-     * We want our pipe to be the specified height
-     */
     for (let i = 1; i <= height; i += 1) {
-      /**
-       * How long will the pipe be after this section is
-       */
       const pipeLength = globals.tileSize * globals.scale * i;
 
       let y;
       if (isOnCeiling) {
-        /**
-         * If the pipe is on the ceiling, its y location should be at the last pipe section
-         */
         y = pipeLength - globals.tileSize;
       } else {
-        /**
-         * If the pipe is on the floor, its y location needs to be calculated from the height of the entire window
-         */
         y = globals.height - pipeLength + globals.tileSize;
       }
 
--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -13,48 +13,24 @@ import Pipes from "../gameObjects/Pipes";
  * Part 2: https://phaser.io/phaser3/devlog/121
  */
 class MainScene extends Phaser.Scene {
-  /**
-   * Scene lifecycle method, preload().
-   * This method is used to load and register assets into our game.
-   */
   preload() {
-    /**
-     * This loads our sprite sheet and assigns it to the key "donuts"
-     */
     // this.load.spritesheet("donuts", donutSpriteSheet, {
     //   frameWidth: globals.tileSize,
     //   frameHeight: globals.tileSize
     // });
   }
 
-  /**
-   * Scene lifecycle method, create().
-   * This is the only required lifecycle method for a Scene.
-   * It is used to initialize your game.
-   */
   create() {
     this.createPlayerInputs();
 
-    /**
-     * Create a donut instance, a Phaser Sprite or an image on the screen
-     */
     // this.donut = new Donut({ scene: this, position: { x: 150, y: 100 } });
 
-    /**
-     * Create a Pipes instance, a Phaser Group or a grouping of similar game objects
-     */
     // this.pipes = new Pipes({ scene: this });
 
-    /**
-     * Enable collisions between our donut and our pipes with the Arcade Physics plugin
-     */
     // this.physics.add.collider(this.donut, this.pipes, (donut, pipe) => {
     //   pipe.body.setGravityY(globals.gravity);
     // });
 
-    /**
-     * Spawn pairs of pipes on a one second loop
-     */
     // this.time.addEvent({
     //   delay: 1000,
     //   callback: this.pipes.buildPipePair,
@@ -102,16 +78,7 @@ class MainScene extends Phaser.Scene {
     // });
   }
 
-  /**
-   * Scene lifecycle method, update().
-   * This method is used to make changes as the game progresses.
-   * It is called when the Scene runs an update step.
-   */
   update() {
-    /**
-     * Game objects don't have lifecycle events by default.
-     * We need to trigger our Pipes' update function explicitly.
-     */
     // this.pipes.update();
   }
 }
